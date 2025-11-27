f4cb839545144701a6cbc0d22fa66a19b2c15cb7
Anderson <anderson@oraculumti.com.br>
2025-11-26 21:58:54 -0300

    add script

---
+++ createusers.ps1
@@ -0,0 +1,37 @@
+#Enter a path to your import CSV file
+$ADUsers = Import-csv C:\Utils\users.csv
+
+foreach ($User in $ADUsers)
+{
+
+       $Username    = $User.username
+       $Password    = $User.password
+       $Firstname   = $User.firstname
+       $Lastname    = $User.lastname
+       $OU           = $User.ou
+
+       #Check if the user account already exists in AD
+       if (Get-ADUser -F {SamAccountName -eq $Username})
+       {
+               #If user does exist, output a warning message
+               Write-Warning "A user account $Username has already exist in Active Directory."
+       }
+       else
+       {
+              #If a user does not exist then create a new user account
+          
+        #Account will be created in the OU listed in the $OU variable in the CSV file; don’t forget to change the domain name in the"-UserPrincipalName" variable
+              New-ADUser `
+            -SamAccountName $Username `
+            -UserPrincipalName "$Username@domain.local" `
+            -Name "$Firstname $Lastname" `
+            -GivenName $Firstname `
+            -Surname $Lastname `
+            -Enabled $True `
+            -ChangePasswordAtLogon $false `
+            -DisplayName "$Firstname $Lastname" `
+            -Path $OU `
+            -AccountPassword (convertto-securestring $Password -AsPlainText -Force)
+
+       }
+}
